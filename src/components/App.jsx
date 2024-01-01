66c929eb6ae6297539b50e5a1d8e042851a1efe7
zShax <71908965+zShax@users.noreply.github.com>
2024-01-01 13:31:27 +0200

    Clean start commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,48 +3,12 @@ import React from 'react';
 // Styles
 import styles from './App.module.css';
 
-// Profile
-import Profile from './Profile/Profile';
-import user from './Profile/user.json';
-
-// Statistics
-import Statistics from './Statistics/Statistics';
-import data from './Statistics/data.json';
-
-// FriendList
-import FriendList from './FriendList/FriendList';
-import friends from './FriendList/friends.json';
-
-// TransactionHistory
-import TransactionHistory from './TransactionHistory/TransactionHistory';
-import transactions from './TransactionHistory/transactions.json';
-
 export const App = () => {
   return (
-    <section className={styles.container}>
-      <article className={styles.containerChild}>
-        <h3>Profile</h3>
-        <Profile
-          username={user.username}
-          tag={user.tag}
-          location={user.location}
-          avatar={user.avatar}
-          stats={user.stats}
-        />
-
-        <h3>Statistics</h3>
-        <Statistics title="upload stats" stats={data} />
-      </article>
-
-      <article className={styles.containerChild}>
-        <h3>Friend List</h3>
-        <FriendList friends={friends} />
-      </article>
-
-      <article className={styles.containerChild}>
-        <h3>Transaction History</h3>
-        <TransactionHistory items={transactions} />
-      </article>
-    </section>
+    <div className={styles.container}>
+      <span>
+        <p>Hello World</p>
+      </span>
+    </div>
   );
 };
